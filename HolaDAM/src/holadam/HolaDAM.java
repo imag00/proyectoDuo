8afe1a566899e2b9cbec0e8a33091923966030a5
Ismael Magro <ismael.magro2000@gmail.com>
2022-03-03 18:37:43 +0100

    AdiosDam

--- HolaDAM/src/holadam/HolaDAM.java
+++ HolaDAM/src/holadam/HolaDAM.java
@@ -4,10 +4,15 @@ public class HolaDAM {
 
     public static void main(String[] args) {
         holaDam();
+        adiosDam();
     }
-    
+
     private static void holaDam() {
         System.out.println("Hola DAM!");
     }
-    
+
+    private static void adiosDam() {
+        System.out.println("Adios DAM!");
+    }
+
 }
