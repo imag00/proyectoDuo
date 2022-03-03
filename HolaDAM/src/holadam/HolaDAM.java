869bb2b805710a7421b8684a03d519bf50611a57
carme <carme@LAPTOP-6AESDREM>
2022-03-03 18:40:11 +0100

    subida segundo desarrollador

--- HolaDAM/src/holadam/HolaDAM.java
+++ HolaDAM/src/holadam/HolaDAM.java
@@ -4,10 +4,14 @@ public class HolaDAM {
 
     public static void main(String[] args) {
         holaDam();
+        llueve();
     }
     
     private static void holaDam() {
         System.out.println("Hola DAM!");
     }
     
+    public static void llueve(){
+        System.out.println("Está lloviendo");
+    } 
 }
