b79a53be01d2151b6d2b2c09ba12830c7de23c41
Ismael Magro <ismael.magro2000@gmail.com>
2022-03-03 18:19:11 +0100

    HolaDam

---
+++ HolaDAM/src/holadam/HolaDAM.java
@@ -0,0 +1,13 @@
+package holadam;
+
+public class HolaDAM {
+
+    public static void main(String[] args) {
+        holaDam();
+    }
+    
+    private static void holaDam() {
+        System.out.println("Hola DAM!");
+    }
+    
+}
